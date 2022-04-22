fe8d0549268e8f110af6cf3e113c1d3bd08c250e
gogogabe9000 <gabe.email1032@yahoo.com>
2022-04-22 19:27:09 -0400

    Added Ending event, working on winning screen

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ let grunt = document.querySelector("#grunt");
 let terrain = document.querySelector("#terrain");
 let cloud = document.querySelector("#cloud");
 let time = document.querySelector("#time");
-let gameOver = document.querySelector("#gameOver");
+let gameOverText = document.querySelector("#gameOverText");
+let gameOverBG = document.querySelector("#gameOverBG")
+let pelican = document.querySelector("#pelican")
 
 //Creating the timer
 let interval = null;
-let playerTime = 61;
+let playerTime = 26;
 
 
 //function for time countdown
 let timeCounter = ()=>{
     playerTime--;
     time.innerHTML = `Time left: <b>${playerTime}</b>s`;
+    if(playerTime == 10){
+        grunt.parentNode.removeChild(grunt)
+    }
+    if(playerTime == 5){
+        pelican.classList.add("pelicanActive")
+    }
 }
 
 
@@ -22,13 +30,14 @@ let timeCounter = ()=>{
 window.addEventListener("keydown", (start)=>{
     if(start.code == "Space")
         {
-            gameOver.style.display = "none";
+            gameOverText.style.display = "none";
+            gameOverBG.style.display = "none";
             grunt.classList.add("gruntActive");
             terrain.firstElementChild.style.animation ="terrainAnimate 1.5s linear infinite";
             cloud.firstElementChild.style.animation ="cloudAnimate 50s linear infinite";
             
             //timer
-            let playerTime = 61;
+            let playerTime = 26;
             interval = setInterval(timeCounter,1000);
         }
 });
@@ -54,12 +63,13 @@ let result = setInterval(() => {
 
     if (masterChiefBottom <= 90 && gruntLeft >= 20 && gruntLeft <= 70) {
 
-        gameOver.style.display = "initial";
+        gameOverText.style.display = "initial";
+        gameOverBG.style.display = "block";
         grunt.classList.remove("gruntActive");
         terrain.firstElementChild.style.animation = "none";
         cloud.firstElementChild.style.animation = "none";
         clearInterval(interval);
-        playerTime = 61;
+        playerTime = 26;
     }
 }, 10);
 
